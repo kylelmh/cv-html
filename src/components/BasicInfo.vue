211fe0745335e3ce3641e9b724d3e5914d057402
kylelmh <kyle.lhe@gmail.com>
2023-07-03 13:03:47 +0900

    updated CV

--- src/components/BasicInfo.vue
+++ src/components/BasicInfo.vue
@@ -1,37 +1,34 @@
 <template>
-<div>
-  <h1> {{basic_info.name}} </h1>
-  <div class="flexbox">
-    <div>
-      <i class="bi-envelope -inline"></i>
-      <a class="u-ml5 -inline" :href="'mailto:'+ basic_info.email"> {{basic_info.email}} </a>
-    </div>
-    <div>
-      <i class="bi-telephone-fill"></i>
-      <a class="u-ml5" :href="'tel:' + basic_info.phone">{{basic_info.phone}}</a>
-    </div>
-    <div>
-     <i class="bi-github"></i>
-     <a class="u-ml5" :href="'https://www.github.com/' + basic_info.github">github.com/{{basic_info.github}}</a>
-    </div>
-    <div>
-     <i class="bi-link-45deg"></i>
-      <a class="u-ml5" :href="'https://' + basic_info.githubio + '.github.io/cv-html'">{{basic_info.githubio}}.github.io/cv-html</a>
+  <div>
+    <h1> {{ basic_info.name }} </h1>
+    <div class="flexbox">
+      <div>
+        <i class="bi-envelope -inline"></i>
+        <a class="u-ml5 -inline" :href="'mailto:' + basic_info.email"> {{ basic_info.email }} </a>
+      </div>
+      <div>
+        <i class="bi-github"></i>
+        <a class="u-ml5" :href="'https://www.github.com/' + basic_info.github">github.com/{{ basic_info.github }}</a>
+      </div>
+      <div>
+        <i class="bi-link-45deg"></i>
+        <a class="u-ml5"
+          :href="'https://' + basic_info.githubio + '.github.io/cv-html'">{{ basic_info.githubio }}.github.io/cv-html</a>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
+  data() {
+    return {
       basic_info: {}
     }
   },
-  beforeCreate(){
+  beforeCreate() {
     import(`../json/${this.$i18n.locale}/basic_info.json`)
-    .then( (e) => {this.basic_info = e.default} )
+      .then((e) => { this.basic_info = e.default })
   }
 }
 </script>
